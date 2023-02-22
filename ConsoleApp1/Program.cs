0a77cac457be7acd8bb475521c63044ae4b0390e
PeterHanson33 <maildotnet@gmx.de>
2023-02-22 18:04:08 +0100

    Initial commit.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using log4net;
+using log4net.Config;
+
+XmlConfigurator.Configure(new FileInfo("log4net.config"));
+
+long i = 0;
+
+var log = LogManager.GetLogger(typeof(Program));
+
+while (true)
+{
+    log.Info($"[{i++}]: Logging... Waiting 1 second...\n");
+    Thread.Sleep(1_000);
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace WpfApp1
+{
+    public partial class MainWindow
+    {
+        private readonly FileSystemWatcher _watcher;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var filePath = "D:/file.txt";
+
+            string directoryName = Path.GetDirectoryName(filePath);
+            
+            _watcher = new FileSystemWatcher
+            {
+                Path = directoryName!,
+                Filter = Path.GetFileName(filePath)!,
+                NotifyFilter = NotifyFilters.Attributes
+                               | NotifyFilters.CreationTime
+                               | NotifyFilters.FileName
+                               | NotifyFilters.LastAccess
+                               | NotifyFilters.LastWrite
+                               | NotifyFilters.Security
+                               | NotifyFilters.Size
+            };
+            _watcher.Error += WatcherOnError;
+            _watcher.Changed += OnWatcherOnChanged;
+            _watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnWatcherOnChanged(object sender, FileSystemEventArgs e)
+        {
+            Box.Dispatcher.Invoke(() =>
+            {
+                Box.Text = DateTime.Now.ToString("HH:mm:ss.fff") + "-" + e.ChangeType + "-" + e.Name + "-" +
+                           e.ChangeType + Environment.NewLine + Box.Text;
+            });
+        }
+
+        private void WatcherOnError(object sender, ErrorEventArgs e)
+        {
+            Box.Dispatcher.Invoke(() =>
+            {
+                Box.Text = DateTime.Now.ToString("HH:mm:ss.fff") + "ERROR" + "-" + e.GetException() + Box.Text;
+            });
+        }
+    }
+}
